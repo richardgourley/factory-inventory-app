aa2fb97d377728cb9e7a793947e403ff5adf6865
unknown <codeleap@outlook.com>
2019-09-26 16:28:25 +0200

    App setup class added.

---
+++ classes/appSetUp.php
@@ -0,0 +1,16 @@
+<?php
+require_once( 'dbConnection.php' );
+
+class AppSetUp extends DbConnection{
+    private function check_users_exist(){
+        
+    }
+    
+    private function insert_user( $user_details ){
+        
+    }
+
+    public function app_set_up(){
+        
+    }
+}
