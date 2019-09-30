81bde90ee9df53a1f4eb04714b0b7ef7f2678c97
unknown <codeleap@outlook.com>
2019-09-30 19:48:10 +0200

    Check fields error log added to login template.

--- templates/login.php
+++ templates/login.php
@@ -1,4 +1,5 @@
 <?php require_once( 'header.php' ); ?>
+<?php require_once( htmlspecialchars( $_SERVER['DOCUMENT_ROOT'] ) . '/factoryInventoryApp/classes/logIn.php' ); ?>
 
 <form method="post" action="<?php echo htmlentities( $_SERVER['PHP_SELF'], ENT_QUOTES ); ?>">
   <table class="form-table">
@@ -28,7 +29,8 @@
   </table>
 </form> 
 
+<?php $log_in = new LogIn(); ?>
+<small><?php $log_in->check_fields(); ?></small>
 <?php require_once( 'footer.php' ); ?>
-
 
 
