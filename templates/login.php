b25861300d7270260bdf8da951cd2d69deaa43b3
unknown <codeleap@outlook.com>
2019-09-30 17:33:49 +0200

    Form fields added to login form

--- templates/login.php
+++ templates/login.php
@@ -1,14 +1,30 @@
 <?php require_once( 'header.php' ); ?>
 
-<?php //<form method="post" action="" 
-?>
-
-<?php echo htmlentities( $_SERVER['PHP_SELF'], ENT_QUOTES ); ?>
-
 <form method="post" action="<?php echo htmlentities( $_SERVER['PHP_SELF'], ENT_QUOTES ); ?>">
   <table class="form-table">
-  	<tbody>
-  	</tbody>
+    <tbody>
+      <tr>
+        <th>
+          Username:
+        </th>
+        <td>
+          <input type="text" name="username" value="">
+        </td>
+      </tr>
+        <th>
+          Password:
+        </th>
+        <td>
+          <input type="password" name="pword" value="">
+        </td>
+      <tr>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" value="Log In">
+        </td>
+      </tr>
+    </tbody>
   </table>
 </form> 
 
