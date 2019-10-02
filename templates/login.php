c9a852b433492c99b12d5c16aefb5eeb04e01af7
unknown <codeleap@outlook.com>
2019-10-02 19:32:54 +0200

    Button class added to form input.

--- templates/login.php
+++ templates/login.php
@@ -1,36 +1,39 @@
 <?php require_once( 'header.php' ); ?>
-<?php require_once( htmlspecialchars( $_SERVER['DOCUMENT_ROOT'] ) . '/factoryInventoryApp/classes/logIn.php' ); ?>
 
-<form method="post" action="<?php echo htmlentities( $_SERVER['PHP_SELF'], ENT_QUOTES ); ?>">
+<?php if( isset( $_SESSION['ERROR_MESSAGE'] ) ): ?>
+<small class="error-message"><?php echo htmlentities( $_SESSION['ERROR_MESSAGE'] ) ?></small>
+<?php endif; ?>
+
+<form method="post" 
+action="../logInAction.php">
   <table class="form-table">
-    <tbody>
-      <tr>
-        <th>
-          Username:
-        </th>
-        <td>
-          <input type="text" name="username" value="">
-        </td>
-      </tr>
-        <th>
-          Password:
-        </th>
-        <td>
-          <input type="password" name="pword" value="">
-        </td>
-      <tr>
-      </tr>
-      <tr>
-        <td>
-          <input type="submit" value="Log In">
-        </td>
-      </tr>
-    </tbody>
+  	<tbody>
+  		<tr>
+  		  <th>
+  		  	Username:
+  		  </th>
+  		  <td>
+  		  	<input type="text" name="username" value="">
+  		  </td>
+  		</tr>
+  		  <th>
+  		  	Password:
+  		  </th>
+  		  <td>
+  		  	<input type="password" name="pword" value="">
+  		  </td>
+  		<tr>
+  		</tr>
+  		<tr>
+  		  <td>
+  		  	<input class="button" type="submit" value="Log In">
+  		  </td>
+  		</tr>
+  	</tbody>
   </table>
 </form> 
 
-<?php $log_in = new LogIn(); ?>
-<small><?php $log_in->check_fields(); ?></small>
 <?php require_once( 'footer.php' ); ?>
+
 
 
