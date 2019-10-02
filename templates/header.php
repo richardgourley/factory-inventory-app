af7995cc2dbe53ebbe642c055d7697d34594b5ba
unknown <codeleap@outlook.com>
2019-10-02 20:48:32 +0200

    Conditional menu added to header.

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,9 @@
 
     <?php if( isset( $_SESSION['USERNAME'] ) ): ?>
     <h3>Welcome back <?php echo htmlentities( $_SESSION['USERNAME'], ENT_QUOTES ); ?></h3>
+    <?php endif; ?>
 
+    <?php if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '1' ): ?>
     <ul>
     	<li><a href="<?php echo BASE_URL . 'index.php'; ?>">Home</a></li>
         <li><a href="<?php echo BASE_URL . 'templates/viewAll.php'; ?>">View all products</a></li>
@@ -30,6 +32,13 @@
     	<li><a href="<?php echo BASE_URL . 'templates/search.php'; ?>">Search</a></li>
     	<li><a href="<?php echo BASE_URL . 'templates/logout.php'; ?>">Logout</a></li>
     </ul>
+    <?php elseif( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '2' ): ?>
+    <ul>
+        <li><a href="<?php echo BASE_URL . 'index.php'; ?>">Home</a></li>
+        <li><a href="<?php echo BASE_URL . 'templates/viewAll.php'; ?>">View all products</a></li>
+        <li><a href="<?php echo BASE_URL . 'templates/search.php'; ?>">Search</a></li>
+        <li><a href="<?php echo BASE_URL . 'templates/logout.php'; ?>">Logout</a></li>
+    </ul> 
     <?php else: ?>
     <p>You are not currently logged in.</p>
     <ul>
