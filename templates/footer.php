60bb3f7738dd5224baa4481f1090c6510ea37790
unknown <codeleap@outlook.com>
2019-10-02 20:49:35 +0200

    Footer template added.

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<?php require_once( htmlspecialchars($_SERVER['DOCUMENT_ROOT']) . '/factoryInventoryApp/config/config.php' ); ?> 
+  <script type="text/javascript" src="<?php echo BASE_URL . 'assets/main.js'; ?>"></script>
+</body>
+</html>
+
+
