8edf73c6836bb38b1cdf9238a854c7936a0efd93
unknown <codeleap@outlook.com>
2019-10-03 19:51:12 +0200

    Model class created and view_all method called in viewAll template.

--- templates/viewAll.php
+++ templates/viewAll.php
@@ -1,5 +1,14 @@
 <?php require_once( 'header.php' ); ?>
+<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/Model.php' ); ?>
+
+<?php 
+$model = new Model(); 
+$results = $model->view_all();
+?>
+
 <?php require_once( 'footer.php' ); ?>
+
+
 
 
 
