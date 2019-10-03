5e6d3306fc4d46cc47adadacc1c008f7f3419052
unknown <codeleap@outlook.com>
2019-10-03 19:36:20 +0200

    Restrictions based on user priveliges added to edit template.

--- templates/edit.php
+++ templates/edit.php
@@ -1,3 +1,14 @@
+<?php 
+if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] == '2' ){
+    header( "Location:" . $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/index.php' );
+}
+
+if( empty( $_SESSION ) || !isset( $_SESSION['PRIVELIGES'] ) ){
+    header( "Location:" . $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/index.php' );
+}
+
+?>
+
 <?php require_once( 'header.php' ); ?>
 <?php require_once( 'footer.php' ); ?>
 
