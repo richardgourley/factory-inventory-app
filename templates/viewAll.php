60098e545b0389e5551b951f26252f7d1c3e9969
unknown <codeleap@outlook.com>
2019-10-04 20:49:34 +0200

    View all products loop added to viewAll view.

--- templates/viewAll.php
+++ templates/viewAll.php
@@ -1,10 +1,30 @@
 <?php require_once( 'header.php' ); ?>
 <?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/Model.php' ); ?>
 
+<?php if( !isset( $_SESSION['USERNAME'] ) || !isset( $_SESSION['PRIVELIGES'] ) ): ?>
+<h3>Please log in</h3>
+<?php endif; ?>
+<?php //exit(); ?>
+
 <?php 
 $model = new Model(); 
 $results = $model->view_all();
 ?>
+
+<?php var_dump($_SESSION); ?>
+
+<div class="products-grid">
+<?php foreach ( $results as $product ): ?>
+<div class="product">
+<p>PRODUCT NUMBER: <?php echo htmlentities( $product['product_number'], ENT_QUOTES ) ?></p>
+<p>NAME: <?php echo htmlentities( $product['product_name'], ENT_QUOTES ) ?></p>
+<p>DESCRIPTION: <?php echo htmlentities( $product['description'], ENT_QUOTES ) ?></p>
+<p>COST PRICE: <?php echo htmlentities( $product['cost_price'], ENT_QUOTES ) ?></p>
+<p>IN STOCK: <?php echo htmlentities( $product['quantity_in_stock'], ENT_QUOTES ) ?></p>
+</div>
+<?php endforeach; ?>
+</div>
+
 
 <?php require_once( 'footer.php' ); ?>
 
