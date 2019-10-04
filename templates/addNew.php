547032eb342fdf91574b6cee45c7eb841653ae45
unknown <codeleap@outlook.com>
2019-10-04 20:47:52 +0200

    Add product form table added

--- templates/addNew.php
+++ templates/addNew.php
@@ -1,4 +1,6 @@
-<?php 
+<?php require_once( 'header.php' ); ?>
+
+<?php
 if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] == '2' ){
     header( "Location:" . $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/index.php' );
 }
@@ -9,7 +11,62 @@ if( empty( $_SESSION ) || !isset( $_SESSION['PRIVELIGES'] ) ){
 
 ?>
 
-<?php require_once( 'header.php' ); ?>
+
+<div>
+    <h3>Add a product</h3>
+    <form method="post" action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <table class="form-table">
+          <tbody>
+            <tr>
+              <th>
+                Product Number:
+              </th>
+              <td>
+                <input type="text" name="product_number" placeholder="Product Number">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Product Name:
+              </th>
+              <td>
+                <input type="text" name="product_name" placeholder="Product Name">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Description:
+              </th>
+              <td>
+                <input type="text" name="description" placeholder="Description">
+              </td>
+            </tr> 
+            <tr>
+              <th>
+                Cost Price:
+              </th>
+              <td>
+                <input type="text" name="cost_price" placeholder="Cost Price">
+              </td>
+            </tr> 
+            <tr>
+              <th>
+                Quantity In Stock:
+              </th>
+              <td>
+                <input type="text" name="quantity_in_stock" placeholder="Quantity in Stock">
+              </td>
+            </tr>  
+            <tr>
+              <td>
+                <input type="submit" name="submit" value="Add Product">
+              </td>
+            </tr> 
+          </tbody>
+        </table>
+    </form>
+</div>
+
 <?php require_once( 'footer.php' ); ?>
 
 
