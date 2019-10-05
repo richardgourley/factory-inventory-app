df785a78c61630f42d164788f2356bbab36d9e24
unknown <codeleap@outlook.com>
2019-10-05 14:34:57 +0200

    Break statements added to error messages returned.

--- classes/DataValidationSanitization.php
+++ classes/DataValidationSanitization.php
@@ -8,20 +8,20 @@ class DataValidationSanitization{
     public function is_whole_number( $var, $input_name ){
 
         if( is_numeric( $var ) && strpos( $var, '.') == false ){
-            return;
+            return '';
         }
 
-        return 'Your ' . $input_name . ' is not a valid number';
+        return 'Your ' . $input_name . ' is not a valid number.<br>';
 
     }
 
     public function is_a_string( $var, $input_name ){
         if( is_numeric( $var ) || is_bool( $var ) ){
-            return 'Your ' . $input_name . ' is invalid';
+            return 'Your ' . $input_name . ' is invalid.<br>';
         }
 
         if( is_string( $var ) ){
-            return;
+            return '';
         }
     }
 
@@ -29,19 +29,19 @@ class DataValidationSanitization{
         $pattern = "/^\d+(\.\d{2})?$/";
 
         if( preg_match( $pattern, $var ) == 1 ){
-            return;
+            return '';
         }
 
-        return 'Your ' . $input_name . ' should be a whole number or a number with 2 decimal digits'; 
+        return 'Your ' . $input_name . ' should be a whole number or a number with 2 decimal digits.<br>'; 
     }
 
     public function is_blank_field( $post ){
         foreach( $post as $field ){
             if( $field === '' ){
-                return 'One or more of your fields was blank.';
+                return 'One or more of your fields was blank.<br>';
             }
         }
 
-        return;
+        return '';
     }
 }
