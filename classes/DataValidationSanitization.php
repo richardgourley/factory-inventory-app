bf6a662f8f69b9c331e343331cbab86ec9b260bb
unknown <codeleap@outlook.com>
2019-10-05 14:29:37 +0200

    is_a_string method added.

--- classes/DataValidationSanitization.php
+++ classes/DataValidationSanitization.php
@@ -8,10 +8,40 @@ class DataValidationSanitization{
     public function is_whole_number( $var, $input_name ){
 
         if( is_numeric( $var ) && strpos( $var, '.') == false ){
-            return '';
+            return;
         }
 
         return 'Your ' . $input_name . ' is not a valid number';
 
+    }
+
+    public function is_a_string( $var, $input_name ){
+        if( is_numeric( $var ) || is_bool( $var ) ){
+            return 'Your ' . $input_name . ' is invalid';
+        }
+
+        if( is_string( $var ) ){
+            return;
+        }
+    }
+
+    public function is_price( $var, $input_name ){
+        $pattern = "/^\d+(\.\d{2})?$/";
+
+        if( preg_match( $pattern, $var ) == 1 ){
+            return;
+        }
+
+        return 'Your ' . $input_name . ' should be a whole number or a number with 2 decimal digits'; 
+    }
+
+    public function is_blank_field( $post ){
+        foreach( $post as $field ){
+            if( $field === '' ){
+                return 'One or more of your fields was blank.';
+            }
+        }
+
+        return;
     }
 }
