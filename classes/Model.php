c79a0773fcdf89bd214e5321e8bfc1ffdf8686c3
unknown <codeleap@outlook.com>
2019-10-05 14:48:56 +0200

    add_product method - success or error message returned after inserting product

--- classes/Model.php
+++ classes/Model.php
@@ -7,15 +7,35 @@ class Model extends DbConnection{
         $conn = $this->create_connection();
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         $query = "SELECT * FROM products";
-        $handler = $conn->prepare( $query );
-        $handler->execute();
-        $results = $handler->fetchAll( PDO::FETCH_ASSOC );
+        $handle = $conn->prepare( $query );
+        $handle->execute();
+        $results = $handle->fetchAll( PDO::FETCH_ASSOC );
  
         $conn = null;
         return $results;
     }
 
-    public function add_product(){
+    public function add_product( $post ){
     	$conn = $this->create_connection();
+    	
+		$query = "INSERT INTO products
+		(product_number, product_name, description, cost_price, quantity_in_stock) 
+		VALUES(?,?,?,?,?)";
+		$handle = $conn->prepare( $query );
+		$handle->bindValue( 1, $post['product_number'] );
+		$handle->bindValue( 2, $post['product_name'] );
+		$handle->bindValue( 3, $post['description'] );
+		$handle->bindValue( 4, $post['cost_price'] );
+		$handle->bindValue( 5, $post['quantity_in_stock'] );
+		
+		$result = $handle->execute();
+
+        if( $result ){
+        	return 'Product sucsessfully added.';
+        }else{
+        	return 'There was an error, your product was not added. Please try again.';
+        }
+		
+
     }
 }
