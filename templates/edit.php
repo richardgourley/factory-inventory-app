228e5bea8aa53c64271b67d9eab979f0f71ce062
unknown <codeleap@outlook.com>
2019-10-05 15:12:54 +0200

    Model and data validation classes added

--- templates/edit.php
+++ templates/edit.php
@@ -1,4 +1,12 @@
 <?php require_once( 'header.php' ); ?>
+<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/Model.php' ); ?>
+<?php 
+require_once( 
+	$_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/DataValidationSanitization.php'  
+); 
+?>
+
+
 <?php 
 
 if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] == '2' ){
@@ -10,6 +18,107 @@ if( empty( $_SESSION ) || !isset( $_SESSION['PRIVELIGES'] ) ){
 }
 
 ?>
+
+<?php 
+$model = new Model();
+$products = $model->view_all();
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset( $_POST['product_number'] ) ){
+    $input_checks = new DataValidationSanitization();
+    $errors = '';
+    $post = filter_var_array( $_POST, FILTER_SANITIZE_STRING );
+
+    //blank fields
+    $errors .= $input_checks->is_blank_field( $post );
+    
+    //data validation
+
+    $errors .= $input_checks->is_whole_number( $post['product_number'], 'Product Number' );
+    $errors .= $input_checks->is_a_string( $post['product_name'], 'Product Name' );
+    $errors .= $input_checks->is_a_string( $post['description'], 'Description' );
+    $errors .= $input_checks->is_price( $post['cost_price'], 'Cost Price' );
+    $errors .= $input_checks->is_whole_number( $post['quantity_in_stock'], 'Quanitiy in Stock' );
+
+
+    if( strlen( $errors) > 0 ){
+        echo '<h4>Please review these errors in your form</h4>';
+        echo '<p>' . $errors . '</p>';
+    }else{
+        echo $model->update_product( $post ); //returns 'product added ' or error message
+        //refresh products
+        $products = $model->view_all();
+    }
+}
+?>
+
+<?php foreach( $products as $product ): ?>
+<div>
+    <h3>Edit a product</h3>
+    <form method="post" action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <table class="form-table">
+          <tbody>
+            <tr>
+              <th>
+                Product Number:
+              </th>
+              <td>
+                <input type="text" name="product_number" placeholder="Product Number" 
+                value="<?php echo htmlentities( $product['product_number'], ENT_QUOTES ); ?>">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Product Name:
+              </th>
+              <td>
+                <input type="text" name="product_name" placeholder="Product Name"
+                value="<?php echo htmlentities( $product['product_name'], ENT_QUOTES ); ?>">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Description:
+              </th>
+              <td>
+                <input type="text" name="description" placeholder="Description"
+                value="<?php echo htmlentities( $product['description'], ENT_QUOTES ); ?>">
+              </td>
+            </tr> 
+            <tr>
+              <th>
+                Cost Price:
+              </th>
+              <td>
+                <input type="text" name="cost_price" placeholder="Cost Price"
+                value="<?php echo htmlentities( $product['cost_price'], ENT_QUOTES ); ?>">
+              </td>
+            </tr> 
+            <tr>
+              <th>
+                Quantity In Stock:
+              </th>
+              <td>
+                <input type="text" name="quantity_in_stock" placeholder="Quantity in Stock"
+                value="<?php echo htmlentities( $product['quantity_in_stock'], ENT_QUOTES ); ?>">
+              </td>
+            </tr> 
+            <tr>
+              <td>
+                <input type="hidden" name="id"
+                value="<?php echo htmlentities( $product['id'], ENT_QUOTES ); ?>">
+              </td>
+            </tr> 
+            <tr>
+              <td>
+                <input type="submit" name="submit" value="Add Product">
+              </td>
+            </tr> 
+          </tbody>
+        </table>
+    </form>
+</div>
+<?php endforeach; ?>
+
 <?php require_once( 'footer.php' ); ?>
 
 
