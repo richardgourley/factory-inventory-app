65635bd59aac9d61a4b30bcc335df23e0c522992
unknown <codeleap@outlook.com>
2019-10-05 15:11:46 +0200

    Spelling correction

--- classes/Model.php
+++ classes/Model.php
@@ -30,12 +30,38 @@ class Model extends DbConnection{
 		
 		$result = $handle->execute();
 
+		$conn = null;
+
         if( $result ){
-        	return 'Product sucsessfully added.';
+        	return 'Product successfully added.';
         }else{
         	return 'There was an error, your product was not added. Please try again.';
         }
 		
+    }
 
+    public function update_product( $post ){
+        $conn = $this->create_connection();
+    	
+		$query = "UPDATE products 
+		SET product_number = ?, product_name = ?, description = ?, cost_price = ?, quantity_in_stock = ?
+ 		WHERE id = ?";
+		$handle = $conn->prepare( $query );
+		$handle->bindValue( 1, $post['product_number'] );
+		$handle->bindValue( 2, $post['product_name'] );
+		$handle->bindValue( 3, $post['description'] );
+		$handle->bindValue( 4, $post['cost_price'] );
+		$handle->bindValue( 5, $post['quantity_in_stock'] );
+		$handle->bindValue( 6, $post['id'] );
+		
+		$result = $handle->execute();
+
+		$conn = null;
+
+        if( $result ){
+        	return 'Product successfully updated.';
+        }else{
+        	return 'There was an error, your product was not added. Please try again.';
+        }
     }
 }
