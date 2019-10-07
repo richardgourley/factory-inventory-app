730870e9170093c6cf54779f44625fad7f6ff581
unknown <codeleap@outlook.com>
2019-10-07 17:37:46 +0200

    Re-direct added to search page if user is not logged in.

--- templates/search.php
+++ templates/search.php
@@ -1,3 +1,10 @@
 <?php require_once( 'header.php' ); ?>
+
+<?php
+if( empty( $_SESSION ) || !isset( $_SESSION['PRIVELIGES'] ) ){
+    header( "Location:" . ROOT_PATH . '/factoryInventoryApp/index.php' );
+}
+?>
+
 <?php require_once( 'footer.php' ); ?>
 
