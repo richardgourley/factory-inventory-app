aca93c6cbf87c1232eb0aaa285e9bdd7fdb4fcff
unknown <codeleap@outlook.com>
2019-10-07 18:32:55 +0200

    Data validation class required by search.php templates.

--- templates/search.php
+++ templates/search.php
@@ -1,5 +1,6 @@
 <?php require_once( 'header.php' ); ?>
 <?php require_once( ROOT_PATH . 'classes/Model.php'  ); ?>
+<?php require_once( ROOT_PATH . 'classes/DataValidationSanitization.php'  ); ?>
 
 <?php
 if( empty( $_SESSION ) || !isset( $_SESSION['PRIVELIGES'] ) ){
