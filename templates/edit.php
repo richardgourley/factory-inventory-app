0b29aa84589c3b102cbc44b75029ce4c05d0154b
unknown <codeleap@outlook.com>
2019-10-07 18:19:17 +0200

    Document root replaced by root path.

--- templates/edit.php
+++ templates/edit.php
@@ -1,5 +1,5 @@
 <?php require_once( 'header.php' ); ?>
-<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/Model.php' ); ?>
+<?php require_once( ROOT_PATH . 'classes/Model.php' ); ?>
 <?php 
 require_once( 
 	$_SERVER['DOCUMENT_ROOT'] . '/factoryInventoryApp/classes/DataValidationSanitization.php'  
