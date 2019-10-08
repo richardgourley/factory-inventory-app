17549ede7f7fbb2a7bcda2465f614c93f06d8471
unknown <codeleap@outlook.com>
2019-10-08 18:34:19 +0200

    Requiring of config file removed. Base url used in loading js script.

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
-<?php require_once( htmlspecialchars($_SERVER['DOCUMENT_ROOT']) . '/factoryInventoryApp/config/config.php' ); ?> 
-  <script type="text/javascript" src="<?php echo BASE_URL . 'assets/main.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo BASE_URL . 'assets/main.js'; ?>"></script>
 </body>
 </html>
 
