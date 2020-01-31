8032fe147ec4554c7e58846ebb8152b299d7a54e
unknown <codeleap@outlook.com>
2020-01-31 21:01:26 +0100

    Menu separated from main header file.

--- templates/header.php
+++ templates/header.php
@@ -3,44 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 	<title>Factory Inventory App</title>
-    <?php require_once( 
-        htmlentities( $_SERVER['DOCUMENT_ROOT'], ENT_QUOTES ) . '/factoryInventoryApp/config/config.php' 
-    ); ?>
-	<link rel="stylesheet" href="<?php echo BASE_URL . 'assets/style.css'; ?>">
+	<link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
-    <h1>Factory parts inventory app</h1>
-    <?php session_start(); ?>
-
-    <?php if( isset( $_SESSION['USERNAME'] ) ): ?>
-    <h3>Welcome back <?php echo htmlentities( $_SESSION['USERNAME'], ENT_QUOTES ); ?></h3>
-    <?php endif; ?>
-
-    <?php if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '1' ): ?>
-    <ul>
-    	<li><a href="<?php echo BASE_URL . 'index.php'; ?>">Home</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/viewAll.php'; ?>">View all products</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/addNew.php'; ?>">Add new product</a></li>
-    	<li><a href="<?php echo BASE_URL . 'templates/edit.php'; ?>">Edit a product</a></li>
-    	<li><a href="<?php echo BASE_URL . 'templates/search.php'; ?>">Search</a></li>
-    	<li><a href="<?php echo BASE_URL . 'templates/logout.php'; ?>">Logout</a></li>
-    </ul>
-    <?php elseif( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '2' ): ?>
-    <ul>
-        <li><a href="<?php echo BASE_URL . 'index.php'; ?>">Home</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/viewAll.php'; ?>">View all products</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/search.php'; ?>">Search</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/logout.php'; ?>">Logout</a></li>
-    </ul> 
-    <?php else: ?>
-    <p>You are not currently logged in.</p>
-    <ul>
-        <li><a href="<?php echo BASE_URL . 'index.php'; ?>">Home</a></li>
-        <li><a href="<?php echo BASE_URL . 'templates/login.php'; ?>">Login</a></li>
-    </ul>
-    <?php endif; ?>
-    
-    
-
-
+    <h1>Factory Inventory App</h1>
 
