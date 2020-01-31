8dac651015906f0a4aa647a5233ae7c3ed32aab4
unknown <codeleap@outlook.com>
2020-01-31 21:00:13 +0100

    Index changed to guide user to app set up page if db doesnt exist.

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-<?php require_once( 'config/config.php' ); ?>
-<?php require_once( 'templates/header.php' ); ?>
-<?php require_once( 'classes/appSetUp.php' ); ?>
+<?php 
+require_once( 'templates/header.php' );
+require_once( 'classes/appSetUp.php' ); 
 
-<?php
-//checks passwords are encrypted and encrypts if necessary
-$app = new AppSetUp();
-$app->hash_plain_passwords();
+
+//create instance of appsetup
+$app_setup = new AppSetUp();
+
+//returns boolean - checks existence of database
+$db_exists = $app_setup->check_db_exists();
+
+if( $db_exists == false ){
+    header("Location:templates/appSetUp");
+}else{
+	require_once( 'templates/header.php' );
+	require_once( 'templates/menu.php' );
+}
 
 if( isset( $_SESSION['ERROR_MESSAGE'] ) ){
     unset( $_SESSION['ERROR_MESSAGE'] );
 }
 
-?>
+require_once( 'templates/footer.php' );
 
-<?php require_once( 'templates/footer.php' ); ?>
+
