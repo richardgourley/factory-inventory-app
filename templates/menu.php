d882a2c25742b57ef8794ff4d0bc5635e6fd43bb
unknown <codeleap@outlook.com>
2020-02-01 14:24:17 +0100

    Menu href links corrected to direct links.

--- templates/menu.php
+++ templates/menu.php
@@ -6,25 +6,25 @@
 
 <?php if( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '1' ): ?>
 <ul>
-    <li><a href="../index.php">Home</a></li>
-    <li><a href="viewAll.php">View all products</a></li>
-    <li><a href="addNew.php">Add new product</a></li>
-    <li><a href="edit.php">Edit a product</a></li>
-    <li><a href="search.php">Search</a></li>
-    <li><a href="logout.php">Logout</a></li>
+    <li><a href="<?php echo SITE_URL . '/index.php'; ?>">Home</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/viewAll.php'; ?>">View all products</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/addNew.php'; ?>">Add new product</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/edit.php'; ?>">Edit a product</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/search.php'; ?>">Search</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/logout.php'; ?>">Logout</a></li>
 </ul>
 <?php elseif( isset( $_SESSION['PRIVELIGES'] ) && $_SESSION['PRIVELIGES'] === '2' ): ?>
 <ul>
-    <li><a href="../index.php">Home</a></li>
-    <li><a href="viewAll.php">View all products</a></li>
-    <li><a href="search.php">Search</a></li>
-    <li><a href="logout.php">Logout</a></li>
+    <li><a href="<?php echo SITE_URL . '/index.php'; ?>">Home</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/viewAll.php'; ?>">View all products</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/search.php'; ?>">Search</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/logout.php'; ?>">Logout</a></li>
 </ul> 
 <?php else: ?>
 <p>You are not currently logged in.</p>
 <ul>
-    <li><a href="../index.php">Home</a></li>
-    <li><a href="login.php">Login</a></li>
+    <li><a href="<?php echo SITE_URL . '/index.php'; ?>">Home</a></li>
+    <li><a href="<?php echo SITE_URL . '/templates/login.php'; ?>">Login</a></li>
 </ul>
 <?php endif; ?>
     
