385a739268542de03dee9b169328405d176f450b
unknown <codeleap@outlook.com>
2020-02-01 14:21:58 +0100

    App setup logic handled in index, checking if db exists, setting up and directing to app setup page.

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once( 'templates/header.php' );
 require_once( 'classes/appSetUp.php' ); 
 
+if( isset( $_SESSION['ERROR_MESSAGE'] ) ){
+    unset( $_SESSION['ERROR_MESSAGE'] );
+}
 
 //create instance of appsetup
 $app_setup = new AppSetUp();
@@ -9,17 +12,28 @@ $app_setup = new AppSetUp();
 //returns boolean - checks existence of database
 $db_exists = $app_setup->check_db_exists();
 
-if( $db_exists == false ){
-    header("Location:templates/appSetUp");
-}else{
-	require_once( 'templates/header.php' );
-	require_once( 'templates/menu.php' );
+//check db exists then check if users with privelige 1 exist in db - if so, display main menu
+if( $db_exists == true ){
+    if( $app_setup->check_main_admin_exists() == true ){
+        require_once( 'templates/menu.php' );
+        exit();
+    }
 }
 
-if( isset( $_SESSION['ERROR_MESSAGE'] ) ){
-    unset( $_SESSION['ERROR_MESSAGE'] );
+//set up db if doesnt exist - first time user of app - go to appSetUp.php
+if( $db_exists == false ){
+    $app_setup->setup_database_and_tables();
+    header("Location:templates/appSetUp.php");
+}
+
+//if db exists but no user exists - go to appSetUp.php
+if( $db_exists == true ){
+    if( $app_setup->check_main_admin_exists() == false ){
+    	header("Location:templates/appSetUp.php");
+    }
 }
 
 require_once( 'templates/footer.php' );
+
 
 
