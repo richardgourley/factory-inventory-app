a953092c09cb28fc4524ea8a5c04274610a252e1
unknown <codeleap@outlook.com>
2020-02-01 14:23:19 +0100

    Site path and url added to header to make available to every page without requiring config file.

--- templates/header.php
+++ templates/header.php
@@ -3,8 +3,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 	<title>Factory Inventory App</title>
-	<link rel="stylesheet" href="../assets/style.css">
+	<?php 
+    if($_SERVER['HTTP_HOST'] == "localhost"){
+    	//for local
+		define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/factory-inventory-app');
+		define('SITEPATH', $_SERVER['DOCUMENT_ROOT'] . '/factory-inventory-app');
+	}
+	else{ 
+		//for web
+		define('SITE_URL', "http://" . $_SERVER['HTTP_HOST']);
+		define('SITEPATH', $_SERVER['DOCUMENT_ROOT']);
+	}
+    ?>
+	<link rel="stylesheet" href="<?php echo SITE_URL . '/assets/style.css'; ?>">
 </head>
 <body>
     <h1>Factory Inventory App</h1>
+    
 
